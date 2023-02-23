14d19f816c52bd28abb530752460ed484ff47879
Xavier Cañizares <xcanizar@vinarytech.com>
2023-02-22 22:26:55 -0500

    finish page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           <p><a href="#home">Home</a></p>
           <p><a href="#whgpt3">What is GPT3</a></p>
           <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#feature">Case Studies</a></p>
+          <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
